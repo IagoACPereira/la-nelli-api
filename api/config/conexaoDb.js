d47184f1bcc747f3477d0286c33ae515b04d649b
Iago Azevedo Costa Pereira <iago0307@gmail.com>
2024-07-04 18:22:20 -0300

    feat: :sparkles: add: Conexão com banco de dados

---
+++ api/config/conexaoDb.js
@@ -0,0 +1,20 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize({
+  database: process.env.DATABASE,
+  port: process.env.PORTADB,
+  username: process.env.USUARIODB,
+  password: process.env.SENHADB,
+  host: process.env.HOSTDB,
+  dialect: 'postgres',
+  logging: false,
+});
+
+try {
+  sequelize.authenticate();
+  console.log('Conexão com o banco de dados estabelecida com exito');
+} catch (error) {
+  console.log('Erro ao conectar com o banco de dados');
+}
+
+export default sequelize;
