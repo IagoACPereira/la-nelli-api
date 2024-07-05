af1826318022300364d35b81a1fa6728f90e06bc
Iago Azevedo Costa Pereira <iago0307@gmail.com>
2024-07-05 13:06:30 -0300

    feat: :sparkles: add: Criado modelo para sclientes

---
+++ api/models/Clientes.js
@@ -0,0 +1,26 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/conexaoDb.js';
+
+const Clientes = sequelize.define('clientes', {
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  telefone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  endereco: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  timestamps: false,
+  freezeTableName: true,
+});
+
+export default Clientes;
