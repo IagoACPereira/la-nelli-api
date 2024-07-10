acc00982a65493725eb64d1db462afe2ace6cdac
Iago Azevedo Costa Pereira <iago0307@gmail.com>
2024-07-10 12:46:10 -0300

    feat: :sparkles: add: Adicionado rotas dos modelos na rota principal

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import statusPedidos from './StatusPedidosRoutes.js';
+import clientes from './ClientesRoutes.js';
+import cargosFuncionarios from './CargosFuncionariosRoutes.js';
+import categoriasProdutos from './CategoriasProdutosRoutes.js';
+import fornecedores from './FornecedoresRoutes.js';
+import produtos from './ProdutosRoutes.js';
+import funcionarios from './FuncionariosRoutes.js';
+import pedidos from './PedidosRoutes.js';
+import itensPedidos from './ItensPedidosRoutes.js';
+import produtosFornecedores from './ProdutosFornecedoresRoutes.js';
+import registroCompraFornecedor from './RegistroCompraFornecedorRoutes.js';
 
 function router(app) {
   app
@@ -10,7 +21,20 @@ function router(app) {
     )
     .get('/', (_, res) => {
       res.json('La Nelli API');
-    });
+    })
+    .use(
+      statusPedidos,
+      clientes,
+      cargosFuncionarios,
+      categoriasProdutos,
+      fornecedores,
+      produtos,
+      funcionarios,
+      pedidos,
+      itensPedidos,
+      produtosFornecedores,
+      registroCompraFornecedor,
+    );
 }
 
 export default router;
