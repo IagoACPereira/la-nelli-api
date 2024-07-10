cb00348cb273007aac597b3fb32a173b6b6c04d9
Iago Azevedo Costa Pereira <iago0307@gmail.com>
2024-07-10 18:35:16 -0300

    feat: :sparkles: add: [GET] Adicionado retorno de apenas um registro da tabela de status dos pedidos

--- api/controllers/StatusPedidosController.js
+++ api/controllers/StatusPedidosController.js
@@ -26,8 +26,13 @@ class StatusPedidosController {
   }
 
   static async exibirUm(req, res) {
+    const { id } = req.params;
     try {
-      res.status(200).json('Em Desenvolvimento.');
+      const statusPedido = await StatusPedidos.findOne({
+        where: { id },
+      });
+
+      res.status(200).json(statusPedido);
     } catch (error) {
       res.status(400).json({
         mensagem: error.message,
