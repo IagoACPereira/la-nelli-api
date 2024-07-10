cacaf041df6d2f57b799421c40d26a0bad847048
Iago Azevedo Costa Pereira <iago0307@gmail.com>
2024-07-10 18:34:21 -0300

    feat: :sparkles: add: [GET] Adicionado retorno de apenas um registro da tabela de produtos

--- api/controllers/ProdutosController.js
+++ api/controllers/ProdutosController.js
@@ -26,8 +26,13 @@ class ProdutosController {
   }
 
   static async exibirUm(req, res) {
+    const { id } = req.params;
     try {
-      res.status(200).json('Em Desenvolvimento.');
+      const produto = await Produtos.findOne({
+        where: { id },
+      });
+
+      res.status(200).json(produto);
     } catch (error) {
       res.status(400).json({
         mensagem: error.message,
