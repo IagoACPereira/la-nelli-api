3bdfa0280988289918dee0980123e1492579cc26
Iago Azevedo Costa Pereira <iago0307@gmail.com>
2024-07-10 16:37:14 -0300

    feat: :sparkles: add: [GET] Adicionado retorno de todos os dados da tabela de itens dos pedidos

--- api/controllers/ItensPedidosController.js
+++ api/controllers/ItensPedidosController.js
@@ -1,3 +1,5 @@
+import ItensPedidos from '../models/ItensPedidos.js';
+
 class ItensPedidosController {
   static async adicionar(req, res) {
     try {
@@ -12,7 +14,9 @@ class ItensPedidosController {
 
   static async exibirTodos(req, res) {
     try {
-      res.status(200).json('Em Desenvolvimento.');
+      const itensPedidos = await ItensPedidos.findAll();
+
+      res.status(200).json(itensPedidos);
     } catch (error) {
       res.status(400).json({
         mensagem: error.message,
