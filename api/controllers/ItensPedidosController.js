e138808e657e47d4ca51dd13b22eeedd35bcf428
Iago Azevedo Costa Pereira <iago0307@gmail.com>
2024-07-10 18:33:26 -0300

    feat: :sparkles: add: [GET] Adicionado retorno de apenas um registro da tabela de itens dos pedidos

--- api/controllers/ItensPedidosController.js
+++ api/controllers/ItensPedidosController.js
@@ -26,8 +26,13 @@ class ItensPedidosController {
   }
 
   static async exibirUm(req, res) {
+    const { id } = req.params;
     try {
-      res.status(200).json('Em Desenvolvimento.');
+      const itemPedido = await ItensPedidos.findOne({
+        where: { id },
+      });
+
+      res.status(200).json(itemPedido);
     } catch (error) {
       res.status(400).json({
         mensagem: error.message,
