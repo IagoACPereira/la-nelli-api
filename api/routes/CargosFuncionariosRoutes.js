eec8333ad3eef1ee6d61b468999055c38d561f9e
Iago Azevedo Costa Pereira <iago0307@gmail.com>
2024-07-10 18:35:38 -0300

    feat: :sparkles: add: [GET] Adicionado retorno de apenas um registro da tabela de cargos dos funcionarios

--- api/routes/CargosFuncionariosRoutes.js
+++ api/routes/CargosFuncionariosRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import CargosFuncionariosController from '../controllers/CargosFincionariosController.js';
+import CargosFuncionariosController from '../controllers/CargosFuncionariosController.js';
 
 const cargosFuncionariosRouter = Router();
 
