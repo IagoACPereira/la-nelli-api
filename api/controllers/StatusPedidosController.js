8943ec6df694dbd1765596302adec2055d8dfeef
Iago Azevedo Costa Pereira <iago0307@gmail.com>
2024-07-10 11:06:08 -0300

    feat: :sparkles: add: Criado controller para status dos pedidos

---
+++ api/controllers/StatusPedidosController.js
@@ -0,0 +1,58 @@
+class StatusPedidosController {
+  static async adicionar(req, res) {
+    try {
+      res.status(201).json('Em Desenvolvimento.');
+    } catch (error) {
+      res.status(400).json({
+        mensagem: error.message,
+        status: 400,
+      });
+    }
+  }
+
+  static async exibirTodos(req, res) {
+    try {
+      res.status(200).json('Em Desenvolvimento.');
+    } catch (error) {
+      res.status(400).json({
+        mensagem: error.message,
+        status: 400,
+      });
+    }
+  }
+
+  static async exibirUm(req, res) {
+    try {
+      res.status(200).json('Em Desenvolvimento.');
+    } catch (error) {
+      res.status(400).json({
+        mensagem: error.message,
+        status: 400,
+      });
+    }
+  }
+
+  static async atualizar(req, res) {
+    try {
+      res.status(200).json('Em Desenvolvimento.');
+    } catch (error) {
+      res.status(400).json({
+        mensagem: error.message,
+        status: 400,
+      });
+    }
+  }
+
+  static async deletar(req, res) {
+    try {
+      res.status(200).json('Em Desenvolvimento.');
+    } catch (error) {
+      res.status(400).json({
+        mensagem: error.message,
+        status: 400,
+      });
+    }
+  }
+}
+
+export default StatusPedidosController;
