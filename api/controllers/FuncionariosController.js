4be46de0ee623f4e11521bca79cd62a57eb49cee
Iago Azevedo Costa Pereira <iago0307@gmail.com>
2024-07-10 18:33:10 -0300

    feat: :sparkles: add: [GET] Adicionado retorno de apenas um registro da tabela de funcionarios

--- api/controllers/FuncionariosController.js
+++ api/controllers/FuncionariosController.js
@@ -26,8 +26,13 @@ class FuncionariosController {
   }
 
   static async exibirUm(req, res) {
+    const { id } = req.params;
     try {
-      res.status(200).json('Em Desenvolvimento.');
+      const funcionario = await Funcionarios.findOne({
+        where: { id },
+      });
+
+      res.status(200).json(funcionario);
     } catch (error) {
       res.status(400).json({
         mensagem: error.message,
